c4f93ac17c4b1201f159cc8ff0fb057c2dde2cb7
Maher Brown <brown.maher42@gmail.com>
2022-12-13 14:27:06 -0500

    Added Spinner

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const form = document.querySelector('form');
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
+    showSpinner();
     // Provides a data structure that behaves like a map
     const data = new FormData(form);
     const response = await fetch('http://localhost:8080/dream', {
@@ -19,5 +20,23 @@ form.addEventListener('submit', async (e) => {
     const { image } = await response.json();
 
     const result = document.querySelector('#result');
-    result.innerHTML = `<img src="${image}" width="512" />`
+    result.innerHTML = `<img src="${image}" width="512" />`;
+    hideSpinner();
 })
+
+function showSpinner() {
+    // Get the button ref
+    const button = document.querySelector('button');
+    // Disable the button
+    button.disabled = true;
+    // Add the spinner
+    button.innerHTML = 'Dreaming... <span class="spinner">🧠</span>'
+}
+function hideSpinner() {
+    // Get the button ref
+    const button = document.querySelector('button');
+    // Enable the button
+    button.disabled = false;
+    // Reset the text
+    button.innerHTML = 'Dream';
+}
