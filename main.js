a8abde62c615265b5e378aeb151082d298e71bde
Maher Brown <brown.maher42@gmail.com>
2022-12-13 14:19:48 -0500

    Copied over styling from example, added js for API

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 import './style.css'
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    // Provides a data structure that behaves like a map
+    const data = new FormData(form);
+    const response = await fetch('http://localhost:8080/dream', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt'),
+        })
+    })
+
+    const { image } = await response.json();
+
+    const result = document.querySelector('#result');
+    result.innerHTML = `<img src="${image}" width="512" />`
+})
